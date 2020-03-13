b71132afab87c74ab36e7d9e0e00c2c6c739b5b3
liudonghua123 <liudonghua123@gmail.com>
2020-03-13 22:33:18 +0800

    initial files created from stagehand full console

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:ldap_passwd/ldap_passwd.dart' as ldap_passwd;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ldap_passwd.calculate()}!');
+}
---
+++ test/ldap_passwd_test.dart
@@ -0,0 +1,8 @@
+import 'package:ldap_passwd/ldap_passwd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
